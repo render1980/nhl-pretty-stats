2b65316fd162a78449d54eff97ac9ee58ee2831f
render1980 <render1980@gmail.com>
2026-03-15 23:10:01 +0200

    Reuse common players related functionality

---
+++ notebooks/players_common.py
@@ -0,0 +1,44 @@
+import requests
+
+BASE_URL = "http://localhost:8080"
+
+def get_player_data(player_id):
+    """Get player landing data by id
+
+    Args:
+        player_id (int)
+
+    Returns:
+        _type_: json-encoded content with player landing info
+    """
+    if player_id:
+        response_stats = requests.get(f"{BASE_URL}/player/{player_id}/stats")
+        if response_stats.status_code == 200:
+            return response_stats.json()
+
+def get_player_info(player_search_pattern):
+    """Get player id by a search pattern
+
+    Args:
+        player_search_pattern (str): Player name to search for
+
+    Returns:
+        _type_: (int, str) or None
+    """
+    if player_search_pattern:
+        response = requests.get(f"{BASE_URL}/player", params={"q": player_search_pattern})
+        if response.status_code == 200:
+            result = response.json()
+            if isinstance(result, list) and len(result) > 0:
+                player_id = result[0]['playerId']
+                player_name = result[0]['name']
+                other_possible_options = [f"\n{p['name']} (ID: {p['playerId']})" for p in result[1:]]
+                print(f"✓ Found Player: {result[0]['name']} (ID: {player_id})")
+                if other_possible_options:
+                    print(f"Other possible players: {', '.join(other_possible_options)}")
+                return (player_id, player_name)
+            else:
+                print(f"✗ Player not found")
+        else:
+            print(f"✗ Error searching Player {player_search_pattern}: {response.status_code}")
+    return None
